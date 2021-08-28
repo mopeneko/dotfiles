b1f2481af8229e445220111d9bc8925c8d372345
mopeneko <mopeneko@lem0n.cc>
2021-08-28 17:02:07 +0900

    Add vim-plug installation

--- preinstall.sh
+++ preinstall.sh
@@ -2,6 +2,9 @@
 
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 if [ "$(uname)" = "Darwin" ]; then
   xcode-select --install
 fi
