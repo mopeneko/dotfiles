6bce41b1f7563645d856dc9a5edc04ef093aa89a
mopeneko <mopeneko@lem0n.cc>
2021-08-28 14:49:43 +0900

    Add shebang

--- preinstall.sh
+++ preinstall.sh
@@ -1,3 +1,5 @@
+#!/bin/sh -ex
+
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
--- preinstall_Linux.sh
+++ preinstall_Linux.sh
@@ -1,1 +1,3 @@
+#!/bin/sh -ex
+
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
