1289c7d4fa46e1630b18f17098bdaaa674ebf541
mopeneko <mopeneko@lem0n.cc>
2021-08-28 14:38:01 +0900

    Add preinstall script

---
+++ preinstall.sh
@@ -0,0 +1,2 @@
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
