68e2408d514c459076efd71ce90407e819a7e668
mopeneko <mopeneko@lem0n.cc>
2021-08-28 14:43:38 +0900

    Auto execute splited preinstall script

--- preinstall.sh
+++ preinstall.sh
@@ -1,2 +1,6 @@
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+if [ -f $HOME/.zshrc_`uname` ]; then
+  . $HOME/.zshrc_`uname`
+fi
