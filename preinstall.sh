e56544255c462a4884f30a5abf92875b6932b0ac
mopeneko <mopeneko@lem0n.cc>
2021-08-28 15:00:51 +0900

    Install Command Line Tools for Xcode before install homebrew if using macOS

--- preinstall.sh
+++ preinstall.sh
@@ -1,6 +1,10 @@
 #!/bin/sh -ex
 
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
+if [ "$(uname)" = "Darwin" ]; then
+  xcode-select --install
+fi
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 if [ -f $HOME/.zshrc_`uname` ]; then
